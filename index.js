84a6893cecd3a4056404a46c90d4a3af73888cac
Brian Jenney <brianjenney83@gmail.com>
2023-08-21 12:25:41 -0700

    test

--- index.js
+++ index.js
@@ -1,37 +1,125 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./router');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const keys = require('./config/keys');
-const passport = require('passport');
-require('./services/passport');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
+const express = require('express');
 const app = express();
+const db = require('@cyclic.sh/dynamodb');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+// var options = {
+//   dotfiles: 'ignore',
+//   etag: false,
+//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+//   index: ['index.html'],
+//   maxAge: '1m',
+//   redirect: false
+// }
+// app.use(express.static('public', options))
+// #############################################################################
+
+// Create or Update an item
+app.post('/:col/:key', async (req, res) => {
+	console.log(req.body);
+
+	const col = req.params.col;
+	const key = req.params.key;
+	console.log(
+		`from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+			req.params
+		)}`
+	);
+	const item = await db.collection(col).set(key, req.body);
+	console.log(JSON.stringify(item, null, 2));
+	res.json(item).end();
+});
+
+// Delete an item
+app.delete('/:col/:key', async (req, res) => {
+	const col = req.params.col;
+	const key = req.params.key;
+	console.log(
+		`from collection: ${col} delete key: ${key} with params ${JSON.stringify(
+			req.params
+		)}`
+	);
+	const item = await db.collection(col).delete(key);
+	console.log(JSON.stringify(item, null, 2));
+	res.json(item).end();
+});
+
+// Get a single item
+app.get('/:col/:key', async (req, res) => {
+	const col = req.params.col;
+	const key = req.params.key;
+	console.log(
+		`from collection: ${col} get key: ${key} with params ${JSON.stringify(
+			req.params
+		)}`
+	);
+	const item = await db.collection(col).get(key);
+	console.log(JSON.stringify(item, null, 2));
+	res.json(item).end();
+});
+
+// Get a full listing
+app.get('/:col', async (req, res) => {
+	const col = req.params.col;
+	console.log(
+		`list collection: ${col} with params: ${JSON.stringify(req.params)}`
+	);
+	const items = await db.collection(col).list();
+	console.log(JSON.stringify(items, null, 2));
+	res.json(items).end();
+});
+
+// Catch all handler for all other request.
+app.use('*', (req, res) => {
+	res.json({ msg: 'no route handler found' }).end();
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`index.js listening on ${port}`);
+});
+
+// const router = require('./router');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const keys = require('./config/keys');
+// const passport = require('passport');
+// require('./services/passport');
+
+// const app = express();
 // app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
-router(app);
+// router(app);
 
-// Server Setup
-const port = process.env.PORT || 8080;
+// // Server Setup
+// const port = process.env.PORT || 8080;
 
-// DB Setup
-mongoose
-	.connect(keys.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log('🚀 DB Connected!');
-		app.listen(port, () => {
-			console.log('😎 Server listening on:', port);
-		});
-	})
-	.catch((err) => {
-		console.log(`❌ DB Connection Error: ${err.message}`);
-	});
+// // DB Setup
+// mongoose
+// 	.connect(keys.MONGO_URI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(() => {
+// 		console.log('🚀 DB Connected!');
+// 		app.listen(port, () => {
+// 			console.log('😎 Server listening on:', port);
+// 		});
+// 	})
+// 	.catch((err) => {
+// 		console.log(`❌ DB Connection Error: ${err.message}`);
+// 	});
