e3c2f9c67a50a6279b5d9f3219cd48fe67366f0b
Brian Jenney <brianjenney83@gmail.com>
2023-08-21 12:10:24 -0700

    do not use cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 require('./services/passport');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
