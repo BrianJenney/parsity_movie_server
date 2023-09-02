8e0e42fc14a5b3c0c43ac882b9f68ebf565dc42e
Brian Jenney <brianjenney83@gmail.com>
2023-09-02 16:21:22 -0700

    remove some functionality

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -15,42 +15,13 @@ const tokenForUser = (user) => {
 };
 
 exports.signin = (req, res) => {
-	res.send({ token: tokenForUser(req.user) });
+	// TODO
 };
 
 exports.currentUser = (req, res) => {
-	const user = {
-		email: req.user.email,
-		token: tokenForUser(req.user),
-		watchListCount: req.user.watchList.length,
-	};
-	res.send(user);
+	// TODO
 };
 
 exports.signup = async (req, res, next) => {
-	const { email, password } = req.body;
-
-	if (!email || !password) {
-		return res
-			.status(422)
-			.send({ error: 'You must provide email and password' });
-	}
-
-	try {
-		const existingUser = await User.findOne({ email });
-
-		if (existingUser) {
-			return res.status(422).send({ error: 'Email is in use' });
-		}
-
-		const user = new User();
-		user.email = email;
-		user.setPassword(password);
-
-		await user.save();
-
-		res.json({ token: tokenForUser(user) });
-	} catch (err) {
-		next(err);
-	}
+	// TODO
 };
--- router.js
+++ router.js
@@ -6,9 +6,6 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function (app) {
-	app.post('/auth/signin', requireSignin, Authentication.signin);
-	app.post('/auth/signup', Authentication.signup);
-	app.get('/auth/current_user', requireAuth, Authentication.currentUser);
 	app.post('/api/watchlist', requireAuth, WatchList.addMovieToList);
 	app.get('/api/watchlist', requireAuth, WatchList.getWatchList);
 };
